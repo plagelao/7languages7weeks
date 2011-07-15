4a3f9e4875c46323741a90167c7638b3516b23fc
Alberto <plagelao@gmail.com>
2011-07-15 08:41:07 +0100

    Fixes an error in erlang tictactoe

--- erlang/day-2/tictactoe.erl
+++ erlang/day-2/tictactoe.erl
@@ -1,21 +1,21 @@
 -module(tictactoe).
 -export([state/1]).
 
-
-state(Board) ->
+state_when_no_winner(Board) ->
   case lists:any(fun(Cell) -> Cell == empty end, Board) of
     true -> no_winner;
-    false -> winner(Board)
+    false -> cat
   end.
 
-winner([C11,C12,C13,
+state(Board) ->
+        [C11,C12,C13,
         C21,C22,C23,
-        C31,C32,C33]) ->
+        C31,C32,C33] = Board,
         Combinations = [[C11,C12,C13],[C21,C22,C23],[C31,C32,C33],
                         [C11,C21,C31],[C12,C22,C32],[C13,C23,C33],
                         [C11,C22,C33],[C13,C22,C31]],
         Solution = [Player || Player <- [x,o], Combination <- Combinations, lists:all(fun(Cell) -> Cell == Player end, Combination)],
         case Solution of
-          [] -> cat;
+          [] -> state_when_no_winner(Board);
           [Winner|_] -> Winner
         end.
