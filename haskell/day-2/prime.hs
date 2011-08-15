455feff2aab93965e0fc0c91ff8220377c319667
Alberto <plagelao@gmail.com>
2011-08-16 00:17:38 +0100

    Fixes a typo

--- haskell/day-2/prime.hs
+++ haskell/day-2/prime.hs
@@ -2,10 +2,10 @@ module Prime where
   primes = 1:2:3:(nextPrimes [3,2])
 
   nextPrimes :: [Int] -> [Int]
-  nextPrimes primesFounded = nextPrime:(nextPrimes (nextPrime:primesFounded))
-    where lastPrime = head primesFounded
+  nextPrimes primesFound = nextPrime:(nextPrimes (nextPrime:primesFound))
+    where lastPrime = head primesFound
           nextPrime = findNextPrime (lastPrime + 2)
           findNextPrime candidate
             | isPrime candidate = candidate
             | otherwise = findNextPrime (candidate + 2)
-          isPrime candidate = all (/=0) (map (mod candidate) primesFounded)
+          isPrime candidate = all (/=0) (map (mod candidate) primesFound)
